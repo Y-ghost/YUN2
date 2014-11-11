3a5a54508a61b1ae315fe71b703e15e29a7e7550
Y-ghost <yangguisongchina@gmail.com>
2014-11-11 10:37:32 +0800

    ygs2014-11-11 10:37:09增加注册登录页面和后台

---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,65 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<style type="text/css">
+.form-signin {
+	width : 500px;
+	margin: 30px auto;
+}
+
+.form-signin div {
+	padding : 15px 0;
+}
+#headLine{
+	display:none;
+}
+</style>
+</head>
+<body>
+	<!-- header -->
+	<jsp:include page="common/header.jsp" />
+	<div class="container">
+		<form class="form-horizontal form-signin" role="form">
+			<div class="text-center"><h2>用户注册</h2></div>
+			<div class="input-group input-group-lg">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span> 
+				<input type="text" class="form-control" placeholder="用户名" name="loginname" id="loginname">
+			</div>
+			<div class="input-group input-group-lg">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span> 
+				<input type="password" class="form-control" placeholder="密码" name="password" id="password">
+			</div>
+			<div class="input-group input-group-lg">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span> 
+				<input type="text" class="form-control" placeholder="邮箱" name="email" id="email">
+			</div>
+			<div class="input-group input-group-lg">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-phone"></span></span> 
+				<input type="text" class="form-control" placeholder="电话" name="phone" id="phone">
+			</div>
+			<div class="input-group input-group-lg">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-map-marker"></span></span> 
+				<input type="text" class="form-control" placeholder="地址" name="address" id="address">
+			</div>
+			<div>
+				<button type="submit" class="btn btn-primary btn-lg btn-block">免费注册</button>
+			</div>
+			<div>
+				<div class="col-sm-6">
+					<input type="checkbox" checked name="serviceAgreement"/>&nbsp;&nbsp;接受<a href="${requestScope.basePath}indexs/login">用户服务协议</a>
+				</div>
+				<div class="col-sm-6 text-right" style="margin-bottom:20%;">
+					已有账号，<a href="${requestScope.basePath}indexs/login">立马登录</a>
+				</div>
+			</div>
+		</form>
+	</div>
+	<!-- footer -->
+	<jsp:include page="common/footer.jsp" />
+	
+	<script src="${requestScope.basePath}js/login.js"></script>
+</body>
+</html>
---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,187 @@
+;
+"user strict";
+
+var rainet = rainet || {};
+rainet.login = rainet.login || {};
+rainet.login.view = function() {
+		var init = function(){
+			rainet.login.controller.register;
+		};
+
+		return {
+			init : init
+		};
+
+}();
+
+
+rainet.login.controller = {
+		// 添加校验信息 当保存或修改project的时候
+		setValidateForUser : function($form){
+			$form.bootstrapValidator({
+				feedbackIcons: {
+		            valid: 'glyphicon glyphicon-ok',
+		            invalid: 'glyphicon glyphicon-remove',
+		            validating: 'glyphicon glyphicon-refresh'
+		        },
+				fields : {
+					loginname : {
+						validators : {
+							notEmpty : {
+								message: '登录名不能为空!'
+							}
+						}
+					},
+					password : {
+						validators : {
+							notEmpty : {
+								message: '密码不能为空!'
+							},
+		                    stringLength: {
+		                        min: 6,
+		                        max: 30,
+		                        message: 'The username must be more than 6 and less than 30 characters long'
+		                    },
+		                    regexp: {
+		                        regexp: /^[^1]+$/i,
+		                        message: '密码不能为空,用于找回密码!'
+		                    }
+						}
+					},
+					email : {
+						validators : {
+							regexp: {
+		                        regexp: /^[^1]+$/i,
+		                        message: '邮箱不能为空,用于找回密码!'
+		                    }
+						}
+					},
+//					serviceAgreement : {
+//						validators : {
+//							regexp: {
+//								regexp: /^[^1]+$/i,
+//								message: '邮箱不能为空,用于找回密码!'
+//							}
+//						}
+//					}
+				}
+			}).on('blur', '#loginname', function(){
+				// 验证项目名称是否存在
+				var bv = $form.data('bootstrapValidator');
+				$field = bv.getFieldElements('loginname');
+				var value = $field.val();
+				if ($.trim(value) === '') {
+					bv.updateMessage($field, 'notEmpty');
+					return ;
+				}
+				var data = {loginname : value};
+				rainet.login.service["User"].validLoginName(data, function(data){
+					if (data) {
+						// 存在，更新错误信息的提示
+						bv.updateMessage($field, 'notEmpty', '登录名称已存在!');
+						bv.updateStatus($field, 'INVALID');
+					}
+				});
+			});
+			
+			$form.data('bootstrapValidator').disableSubmitButtons(true);
+		},
+		register : function(){
+			var $form = $(".form-signin");
+				$('button[type=submit]',$form).off('click').on('click', function(){
+					alert("ss");
+					// 检查验证是否通过
+					var bv = $form.data('bootstrapValidator');
+					if (bv.$invalidFields.length > 0) {
+						return false;
+					}
+					var formData = $form.serializeArray();
+					var jsonData = rainet.utils.serializeObject(formData);
+					// 更新项目
+//					rainet.login.service["User"].register(jsonData, function(data){
+//						if (data) {
+//							rainet.utils.notification.success('注册成功,马上跳转!');
+//							//
+//						}
+//					});
+				});
+				// Add validation
+				this.setValidateForUser($form);
+		}
+};
+
+rainet.login.url = {
+	User : {
+		url : rainet.settings.baseUrl + 'User/'
+	}
+};
+
+
+rainet.login.service = {
+	User : {
+		login : function(param, callback) {
+			rainet.ajax.execute({
+				url : rainet.login.url.User.url+"login/",
+				data : param,
+				$busyEle : $('.container'),
+				method : 'GET',
+				success : function(data) {
+					callback(data);
+				}
+			});
+		},
+		register : function(param, callback) {
+			rainet.ajax.execute({
+				url : rainet.login.url.User.url+"register/",
+				data : param,
+				$busyEle : $('.container'),
+				method : 'POST',
+				success : function(data) {
+					callback(data);
+				}
+			});
+		},
+		sendEmail : function(param, callback) {
+			rainet.ajax.execute({
+				url : rainet.login.url.User.url+"sendEmail/",
+				data : param,
+				$busyEle : $('#passport-title'),
+				method : 'GET',
+				success : function(data) {
+					callback(data);
+				}
+			});
+		},
+		modifyPassword : function(param, callback) {
+			rainet.ajax.execute({
+				url : rainet.login.url.User.url+"modifyPassword/",
+				data : param,
+				$busyEle : $('#passport-title'),
+				method : 'GET',
+				success : function(data) {
+					callback(data);
+				}
+			});
+		},
+		validLoginName : function(param, callback) {
+			rainet.ajax.execute({
+				url : rainet.login.url.User.url+"validLoginName/",
+				data : param,
+				$busyEle : $('#passport-title'),
+				method : 'GET',
+				success : function(data) {
+					callback(data);
+				}
+			});
+		}
+	}
+};
+
+$(document).ready(function() {
+	var myDate = new Date();
+	$(".copyYear").html(myDate.getFullYear());
+	rainet.login.view.init();
+});
+
+
+
